a5451fb914c98a8bbcac271e238e381c6c206c4d
21t0408 <104366780+21t0408@users.noreply.github.com>
2023-06-05 15:12:10 +0200

    Solution cleaned Database findable and show view

--- app.js
+++ app.js
@@ -1,39 +1,31 @@
-const nano = require('nano')('http://admin:admin@127.0.0.1:5984'); // Passen Sie die URL entsprechend an
-const db = nano.db.use('rezepte'); // Passen Sie den Datenbanknamen an
+const express = require('express');  
+const bodyParser = require('body-parser');  
+const path = require('path');  
+const NodeCouchdb = require('nano')('http://sysadm:sysadm@127.0.0.1:5984');
+  
+const couch = NodeCouchdb.db.use('rezepte');
 
-// Beispieloperationen mit der CouchDB
-// Hier können Sie Ihre eigenen Funktionen und Logik einfügen
+couch.list({ include_docs: true }, 
+    (err, body) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        body.rows.forEach(row => {
+            console.log('Dokument:', row.doc);
+        });
+    });
+  
+const app = express();  
+app.set('view engine', 'ejs');  
+app.set('views', path.join(__dirname, 'views'));  
+app.use (bodyParser.json());  
+app.use(bodyParser.urlencoded({extended: false}));  
 
-// Dokument erstellen
-// const document = {
-//   title: 'Mein Dokument',
-//   content: 'Hallo, Welt!'
-// };
+app.get('/', function(req,res){  
+    res.render('index');  
+}); 
 
-// db.insert(document, (err, body) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//   console.log('Dokument erstellt:', body);
-// });
-
-// Dokument abrufen
-// db.get('doc_id', (err, body) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//   console.log('Dokument abgerufen:', body);
-// });
-
-// Alle Dokumente in der Datenbank abrufen
-db.list({ include_docs: true }, (err, body) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  body.rows.forEach(row => {
-    console.log('Dokument:', row.doc);
-  });
-});
+app.listen(3000, function(){  
+ console.log('Server is started on Port 3000');  
+})  
