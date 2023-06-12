95d52dcdb5c670806885f14a31ed65f0a56eec7a
21t0408 <104366780+21t0408@users.noreply.github.com>
2023-06-12 14:40:51 +0200

    View hinzugefuegt

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-const express = require('express');  
-const bodyParser = require('body-parser');  
-const path = require('path');  
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const NodeCouchdb = require('nano')('http://sysadm:sysadm@127.0.0.1:5984');
-  
+
 const couch = NodeCouchdb.db.use('rezepte');
 
-couch.list({ include_docs: true }, 
-    (err, body) => {
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    couch.list({ include_docs: true }, (err, body) => {
         if (err) {
             console.error(err);
             return;
         }
-        body.rows.forEach(row => {
-            console.log('Dokument:', row.doc);
-        });
+        const recipes = body.rows.map(row => row.doc);
+        console.log(recipes)
+        res.render('index', { recipes });
     });
-  
-const app = express();  
-app.set('view engine', 'ejs');  
-app.set('views', path.join(__dirname, 'views'));  
-app.use (bodyParser.json());  
-app.use(bodyParser.urlencoded({extended: false}));  
+});
 
-app.get('/', function(req,res){  
-    res.render('index');  
-}); 
-
-app.listen(3000, function(){  
- console.log('Server is started on Port 3000');  
-})  
+app.listen(3000, function() {
+    console.log('Server is started on Port 3000');
+});
